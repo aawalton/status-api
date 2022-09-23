f2d81b4d0163a8f18501fb8bfc4d799b044f7b9a
Alan Walton <aawalton@gmail.com>
2022-09-23 15:58:37 -0600

    add user query

--- script.ts
+++ script.ts
@@ -3,9 +3,14 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-  // ... you will write your Prisma Client queries here
+  const user = await prisma.user.create({
+    data: {
+      name: 'Alan',
+      email: 'alan@statuspage.ai',
+    },
+  })
+  console.log(user)
 }
-
 main()
   .then(async () => {
     await prisma.$disconnect()
