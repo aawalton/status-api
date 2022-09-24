db2cd8ef3273f70e09b07160806fcd576f056b4e
Alan Walton <aawalton@gmail.com>
2022-09-24 17:49:56 -0600

    more tooling

---
+++ scripts/achievements/wondrium.ts
@@ -0,0 +1,11 @@
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+// import fetch from "node-fetch";
+
+const syncWondriumAchievements = () => {
+  // const response = await fetch("https://www.wondrium.com");
+  console.log("response");
+};
+
+void syncWondriumAchievements();
