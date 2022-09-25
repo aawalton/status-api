f188c0d29b626a76baf8fcd4a8884759f513c47b
Alan Walton <aawalton@gmail.com>
2022-09-24 18:58:45 -0600

    get fetch working

--- src/index.ts
+++ src/index.ts
@@ -1,68 +1,18 @@
-import { ApolloServer, gql } from "apollo-server";
-import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
 import supabase from "./modules/supabase";
-// import syncWondriumAchievements from "./wondrium";
+import fetch from 'node-fetch'
+import parser from 'xml2json'
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
-const typeDefs = gql`
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+const syncWondriumAchievements = async () => {
+  const response = await fetch('https://www.wondrium.com/media/sitemap.xml')
+  const text = await response.text()
+  const json = await parser.toJson(text)
+  const data = JSON.parse(json)
+  const urls = data.urlset.url
+  console.log(urls.length)
 
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
+  const achievements = await supabase.from('achievements').select('*')
+  console.log(achievements)
+}
 
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]
-  }
-`;
 
-const books = [
-  {
-    title: "The Awakening",
-    author: "Kate Chopin",
-  },
-  {
-    title: "City of Glass",
-    author: "Paul Auster",
-  },
-];
-
-// Resolvers define the technique for fetching the types defined in the
-// schema. This resolver retrieves books from the "books" array above.
-const resolvers = {
-  Query: {
-    books: () => books,
-  },
-};
-
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  csrfPrevention: true,
-  cache: "bounded",
-  /**
-   * What's up with this embed: true option?
-   * These are our recommended settings for using AS;
-   * they aren't the defaults in AS3 for backwards-compatibility reasons but
-   * will be the defaults in AS4. For production environments, use
-   * ApolloServerPluginLandingPageProductionDefault instead.
-   * */
-  plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
-});
-
-// The `listen` method launches a web server.
-void server.listen().then(({ url }: { url: string }) => {
-  // eslint-disable-next-line no-console
-  console.log(`🚀  Server ready at ${url}`);
-});
-
-// void syncWondriumAchievements()
+void syncWondriumAchievements()
