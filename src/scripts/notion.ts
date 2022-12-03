0d2b51543375aaf9771c507d121f79991932e664
Alan Walton <aawalton@gmail.com>
2022-12-03 13:25:14 -0700

    get import working with limited fields

--- src/scripts/notion.ts
+++ src/scripts/notion.ts
@@ -4,6 +4,7 @@
 /* eslint-disable no-console */
 import { Client } from '@notionhq/client'
 import dotenv from 'dotenv'
+import _ from 'lodash'
 
 import database from '../modules/database'
 
@@ -20,6 +21,13 @@ type Achievement = {
   id: string
   title: string
   description: string
+  link: string
+  target: number
+  progress: number
+  context: 'passive' | 'active'
+  category_name: 'faith' | 'learn' | 'fun'
+  type: 'collection' | 'sequence' | 'boolean' | 'integer'
+  is_collection: boolean
 }
 
 const addAchievement = async (achievement: Achievement) => {
@@ -45,11 +53,39 @@ const addAchievement = async (achievement: Achievement) => {
     })
   } else {
     /* If the achievement doesn't exist, add it */
-    await notion.databases.query({
-      database_id: ACHIEVEMENTS_DATABASE_ID,
-      filter: {
-        property: 'title',
-        rich_text: { equals: achievement.title },
+    console.log(achievement)
+    await notion.pages.create({
+      parent: { database_id: ACHIEVEMENTS_DATABASE_ID },
+      properties: {
+        Name: {
+          type: 'title',
+          title: [{ type: 'text', text: { content: achievement.title } }],
+        },
+        // Link: { type: 'url', url: achievement.link },
+        ...(!achievement.is_collection
+          ? {
+              Target: {
+                type: 'number',
+                number: achievement.target,
+              },
+              Progress: {
+                type: 'number',
+                number: achievement.progress,
+              },
+            }
+          : {}),
+        // Context: {
+        //   type: 'select',
+        //   select: { name: _.capitalize(achievement.context) },
+        // },
+        // Category: {
+        //   type: 'select',
+        //   select: { name: _.capitalize(achievement.category_name) },
+        // },
+        // Type: {
+        //   type: 'select',
+        //   select: { name: _.capitalize(achievement.type) },
+        // },
       },
     })
   }
@@ -58,7 +94,10 @@ const addAchievement = async (achievement: Achievement) => {
 const run = async () => {
   /* Pull achievements from the database */
   const [achievements] = (await database.query(
-    'select * from achievements limit 1;'
+    `select * from achievements a1
+    where parent_achievement_id is null and imported_at is null
+    or (select imported_at from achievements a2 where a1.parent_achievement_id = a2.id) is not null
+    limit 1;`
   )) as [Achievement[], unknown]
   console.log(achievements.length)
 
