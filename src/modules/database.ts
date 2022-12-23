f05d41b2fc25b92144667351e7a788367bae7c34
Alan Walton <aawalton@gmail.com>
2022-12-23 10:44:41 -0700

    use auto models in sequelize instance

--- src/modules/database.ts
+++ src/modules/database.ts
@@ -1,8 +1,15 @@
 import dotenv from 'dotenv'
 import { Sequelize } from 'sequelize'
 
+import { initModels } from '../../generated/tables/init-models'
+
 dotenv.config()
 
 const sequelize = new Sequelize(process.env.DATABASE_URI ?? '')
 
-export default sequelize
+const database = {
+  sequelize,
+  ...initModels(sequelize),
+}
+
+export default database
