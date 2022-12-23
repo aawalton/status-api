83984aaadf5575464873c7d12458edb1e103629b
Alan Walton <aawalton@gmail.com>
2022-12-23 10:12:20 -0700

    git complete list of courses

--- src/achievements/wondrium.ts
+++ src/achievements/wondrium.ts
@@ -1,3 +1,6 @@
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-shadow */
 /* eslint-disable no-param-reassign */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
@@ -5,176 +8,37 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import puppeteer from 'puppeteer'
 
-import { scrollIntoViewIfNeeded, waitForSelectors } from '../modules/puppeteer'
+import puppeteer from 'puppeteer'
 
 export const signIntoWondrium = async () => {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
-  const timeout = 5000
+  const timeout = 300000
   page.setDefaultTimeout(timeout)
 
+  console.log('set viewport')
   {
     const targetPage = page
-    await targetPage.setViewport({ width: 1146, height: 1311 })
+    await targetPage.setViewport({ width: 1636, height: 1321 })
   }
 
+  console.log('navigate to https://www.wondrium.com/allprograms')
   {
     const targetPage = page
     const promises = []
     promises.push(targetPage.waitForNavigation())
-    await targetPage.goto('https://www.wondrium.com/')
+    await targetPage.goto('https://www.wondrium.com/allprograms')
     await Promise.all(promises)
   }
 
-  {
-    const targetPage = page
-    await scrollIntoViewIfNeeded(
-      [
-        ['#sign-in-btn > a > button > span'],
-        ['xpath///*[@id="sign-in-btn"]/a/button/span'],
-      ],
-      targetPage,
-      timeout
-    )
-    const element = await waitForSelectors(
-      [
-        ['#sign-in-btn > a > button > span'],
-        ['xpath///*[@id="sign-in-btn"]/a/button/span'],
-      ],
-      targetPage,
-      { timeout, visible: true }
-    )
-    await element.click({
-      offset: {
-        x: 15.53057861328125,
-        y: 12.97760009765625,
-      },
-    })
-  }
-  {
-    const targetPage = page
-    await scrollIntoViewIfNeeded(
-      [['aria/Your Email'], ['#email'], ['xpath///*[@id="email"]']],
-      targetPage,
-      timeout
-    )
-    const element = await waitForSelectors(
-      [['aria/Your Email'], ['#email'], ['xpath///*[@id="email"]']],
-      targetPage,
-      { timeout, visible: true }
-    )
-    await element.click({
-      offset: {
-        x: 93.359375,
-        y: 18,
-      },
-    })
-  }
-  {
-    const targetPage = page
-    await scrollIntoViewIfNeeded(
-      [['aria/Your Email'], ['#email'], ['xpath///*[@id="email"]']],
-      targetPage,
-      timeout
-    )
-    const element = await waitForSelectors(
-      [['aria/Your Email'], ['#email'], ['xpath///*[@id="email"]']],
-      targetPage,
-      { timeout, visible: true }
-    )
-    const type = await element.evaluate((el: { type: any }) => el.type)
-    if (['select-one'].includes(type)) {
-      await element.select('aawalton@gmail.com')
-    } else if (
-      [
-        'textarea',
-        'text',
-        'url',
-        'tel',
-        'search',
-        'password',
-        'number',
-        'email',
-      ].includes(type)
-    ) {
-      await element.type('aawalton@gmail.com')
-    } else {
-      await element.focus()
-      await element.evaluate(
-        (
-          el: { value: any; dispatchEvent: (arg0: Event) => void },
-          value: any
-        ) => {
-          el.value = value
-          el.dispatchEvent(new Event('input', { bubbles: true }))
-          el.dispatchEvent(new Event('change', { bubbles: true }))
-        },
-        'aawalton@gmail.com'
-      )
-    }
-  }
-  {
-    const targetPage = page
-    await scrollIntoViewIfNeeded(
-      [['aria/Your Password'], ['#pass'], ['xpath///*[@id="pass"]']],
-      targetPage,
-      timeout
-    )
-    const element = await waitForSelectors(
-      [['aria/Your Password'], ['#pass'], ['xpath///*[@id="pass"]']],
-      targetPage,
-      { timeout, visible: true }
-    )
-    await element.click({
-      offset: {
-        x: 81.359375,
-        y: 19,
-      },
-    })
-  }
-  {
-    const targetPage = page
-    await scrollIntoViewIfNeeded(
-      [['aria/Your Password'], ['#pass'], ['xpath///*[@id="pass"]']],
-      targetPage,
-      timeout
-    )
-    const element = await waitForSelectors(
-      [['aria/Your Password'], ['#pass'], ['xpath///*[@id="pass"]']],
-      targetPage,
-      { timeout, visible: true }
-    )
-    const type = await element.evaluate((el: { type: any }) => el.type)
-    if (['select-one'].includes(type)) {
-      await element.select('bxv@cvp5FPT!ptp0zvu')
-    } else if (
-      [
-        'textarea',
-        'text',
-        'url',
-        'tel',
-        'search',
-        'password',
-        'number',
-        'email',
-      ].includes(type)
-    ) {
-      await element.type('bxv@cvp5FPT!ptp0zvu')
-    } else {
-      await element.focus()
-      await element.evaluate(
-        (
-          el: { value: any; dispatchEvent: (arg0: Event) => void },
-          value: any
-        ) => {
-          el.value = value
-          el.dispatchEvent(new Event('input', { bubbles: true }))
-          el.dispatchEvent(new Event('change', { bubbles: true }))
-        },
-        'bxv@cvp5FPT!ptp0zvu'
-      )
-    }
-  }
+  console.log('find all course links')
+  const hrefs = await page.$$eval('div.course-list a', (as) =>
+    as.map((a) => a.href)
+  )
+  console.log(hrefs.length)
+
+  process.exit(0)
 }
+
+void signIntoWondrium()
