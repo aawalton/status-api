ef06d7376bf9f8ea5393f5154202227bf9583522
Alan Walton <aawalton@gmail.com>
2023-02-04 09:27:26 -0700

    fix type for conqueror achievement

--- src/achievements/conqueror/create.ts
+++ src/achievements/conqueror/create.ts
@@ -3,13 +3,31 @@ import _ from 'lodash'
 import puppeteer from 'puppeteer'
 
 import database from '../../modules/database'
+import { findOrCreateAchievementByTitle } from '../helpers'
+
+type ValidChallenge = { title: string; miles: number; url: string }
 
 const parseDistance = (distance: string) => {
   const imperial = distance.split('/')[0]
   const pieces = imperial.split(' ')
-  const miles = pieces[0].replace(',', '')
+  const miles = pieces[0].replace(',', '').replace('mi', '')
   return _.toNumber(miles)
 }
+
+const createAchievement = (
+  challenge: ValidChallenge,
+  parentAchievementId: string
+) =>
+  findOrCreateAchievementByTitle({
+    title: challenge.title,
+    type: 'integer',
+    categoryName: 'health',
+    formatName: 'automatic',
+    circleName: 'solo',
+    target: challenge.miles,
+    link: challenge.url,
+    parentAchievementId,
+  })
 
 export const getChallenges = async () => {
   /* Set up puppeteer */
@@ -52,7 +70,6 @@ export const getChallenges = async () => {
   )
 
   /* Filter out the invalid challenge cards */
-  type ValidChallenge = { title: string; miles: number; url: string }
   const validChallenges = challenges.filter(
     (challenge) => challenge.title && challenge.miles && challenge.url
   ) as ValidChallenge[]
@@ -63,8 +80,20 @@ export const getChallenges = async () => {
   })
 
   /* Find or create the parent achievement */
+  const parentAchievement = await findOrCreateAchievementByTitle({
+    title: 'Complete All Conqueror Challenges',
+    type: 'sequence',
+    categoryName: 'health',
+    formatName: 'automatic',
+    circleName: 'solo',
+  })
 
   /* Find or create the child achievements */
+  await Promise.all(
+    validChallenges.map((challenge) =>
+      createAchievement(challenge, parentAchievement.id)
+    )
+  )
 
   /* Shut down puppeteer */
   await browser.close()
