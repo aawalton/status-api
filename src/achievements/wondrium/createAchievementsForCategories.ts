9efd2e2c0eaf88404b1137b80b9ffc64388b52fe
Alan Walton <aawalton@gmail.com>
2023-03-04 16:15:41 -0700

    update script for wondrium categories

--- src/achievements/wondrium/createAchievementsForCategories.ts
+++ src/achievements/wondrium/createAchievementsForCategories.ts
@@ -1,16 +1,62 @@
-import { Op } from 'sequelize'
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-restricted-syntax */
+import { literal, Op } from 'sequelize'
 
-import { findOrCreateAchievementForCategory } from './helpers'
+import { sleep } from '../../helpers'
 import database from '../../modules/database'
+import { findOrCreateNotionAchievement } from '../helpers'
+
+const sharedAttributes = {
+  category: 'Learn',
+  format: 'Video',
+  circle: 'Solo',
+  type: 'Collection',
+} as const
 
 export const createAchievementsForCategories = async (): Promise<void> => {
-  const category = await database.wondriumCategories.findOne({
+  /* Create achievement for top level category */
+  const topLevelCategory = await database.wondriumCategories.findOne({
     where: {
-      achievementId: { [Op.is]: undefined },
-      title: { [Op.not]: undefined },
+      parentCategoryId: { [Op.is]: literal('null') },
     },
   })
-  if (!category) return undefined
-  await findOrCreateAchievementForCategory(category)
-  return createAchievementsForCategories()
+  if (!topLevelCategory) throw new Error('No top level category found')
+  const topLevelTitle = `Watch Wondrium ${topLevelCategory.title ?? ''}`
+  await findOrCreateNotionAchievement({
+    title: topLevelTitle,
+    link: topLevelCategory.url,
+    ...sharedAttributes,
+  })
+
+  /* Create achievement for second level categories */
+  const secondLevelCategories = await database.wondriumCategories.findAll({
+    where: { parentCategoryId: topLevelCategory.id },
+  })
+  for (const secondLevelCategory of secondLevelCategories) {
+    const secondLevelTitle = `Watch Wondrium ${secondLevelCategory.title ?? ''}`
+    await findOrCreateNotionAchievement({
+      title: secondLevelTitle,
+      link: topLevelCategory.url,
+      ...sharedAttributes,
+      parentTitle: topLevelTitle,
+    })
+    await sleep(500)
+
+    /* Create achievement for third level categories */
+    const thirdLevelCategories = await database.wondriumCategories.findAll({
+      where: { parentCategoryId: secondLevelCategory.id },
+    })
+    for (const thirdLevelCategory of thirdLevelCategories) {
+      const thirdLevelTitle = `Watch Wondrium ${
+        secondLevelCategory.title ?? ''
+      } > ${thirdLevelCategory.title ?? ''}`
+      await findOrCreateNotionAchievement({
+        title: thirdLevelTitle,
+        link: topLevelCategory.url,
+        ...sharedAttributes,
+        parentTitle: secondLevelTitle,
+      })
+      await sleep(500)
+    }
+  }
 }
--- src/scripts/syncWondriumAchievements.ts
+++ src/scripts/syncWondriumAchievements.ts
@@ -1,15 +1,15 @@
 /* eslint-disable no-console */
 import { createAchievementsForCategories } from '../achievements/wondrium/createAchievementsForCategories'
-import { createAchievementsForCourseCategories } from '../achievements/wondrium/createAchievementsForCourseCategories'
+// import { createAchievementsForCourseCategories } from '../achievements/wondrium/createAchievementsForCourseCategories'
 
 const syncAll = async () => {
   /* Create achievements for categories */
   console.log(new Date(), 'creating achievements for categories')
   await createAchievementsForCategories()
 
-  /* Create achievements for course categories */
-  console.log(new Date(), 'creating achievements for courses')
-  await createAchievementsForCourseCategories()
+  // /* Create achievements for course categories */
+  // console.log(new Date(), 'creating achievements for courses')
+  // await createAchievementsForCourseCategories()
 
   /* Report success */
   process.exit(0)
