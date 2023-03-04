d03c873da9d8e3682ad1a9ca983985c19c941e38
Alan Walton <aawalton@gmail.com>
2023-03-04 07:31:22 -0700

    migrate racquetball achievements

--- src/achievements/exercise/racquetball.ts
+++ src/achievements/exercise/racquetball.ts
@@ -1,39 +1,29 @@
-import { findOrCreateAchievementByTitle } from '../helpers'
+import { findOrCreateNotionAchievement } from '../helpers'
 
 const levels = [10, 100, 1000, 10000, 100000, 1000000]
 
 export const createExerciseAchievements = async () => {
-  /* Find or create parent achievement */
-  const parentAchievement = await findOrCreateAchievementByTitle({
-    title: 'Exercise',
-    type: 'collection',
-    categoryName: 'health',
-    formatName: 'focused',
-    circleName: 'solo',
-  })
-
   /* Find or create achievements for categories */
-  const categoryAchievement = await findOrCreateAchievementByTitle({
-    title: `Play Racquetball`,
-    type: 'sequence',
-    categoryName: 'health',
-    parentAchievementId: parentAchievement.id,
-    formatName: 'focused',
-    circleName: 'with_jenny',
+  await findOrCreateNotionAchievement({
+    title: 'Play Racquetball',
+    type: 'Sequence',
+    category: 'Health',
+    format: 'Focused',
+    circle: 'Jenny',
   })
 
   /* Find or create achievements for levels */
   await Promise.all(
     levels.map((level) =>
-      findOrCreateAchievementByTitle({
+      findOrCreateNotionAchievement({
         title: `Play ${level.toLocaleString()} Games of Racquetball`,
-        type: 'integer',
+        type: 'Integer',
         target: level,
-        level,
-        parentAchievementId: categoryAchievement.id,
-        categoryName: 'health',
-        formatName: 'focused',
-        circleName: 'with_jenny',
+        rank: level,
+        parentTitle: 'Play Racquetball',
+        category: 'Health',
+        format: 'Focused',
+        circle: 'Jenny',
       })
     )
   )
--- src/achievements/helpers.ts
+++ src/achievements/helpers.ts
@@ -1,6 +1,127 @@
+import { Client } from '@notionhq/client'
+import dotenv from 'dotenv'
+
 import { achievements } from '../../generated/tables/achievements'
 import database from '../modules/database'
 
+dotenv.config()
+
+const notion = new Client({
+  auth: process.env.NOTION_TOKEN,
+})
+
+const ACHIEVEMENTS_DATABASE_ID = '06c79efa-24ae-4ff6-a9d2-09bba773934b'
+
+type Achievement =
+  | {
+      title: string
+      target?: number
+      progress?: number
+      type: 'Collection' | 'Sequence'
+      category: 'Faith' | 'Love' | 'Health' | 'Learn' | 'Fun' | 'Wealth'
+      format: 'Audio' | 'Video' | 'Automatic' | 'Focused'
+      circle: 'Solo' | 'Jenny' | 'Group'
+      link?: string
+      rank?: number
+      parentTitle?: string
+    }
+  | {
+      title: string
+      target: number
+      progress?: number
+      type: 'Boolean' | 'Integer'
+      category: 'Faith' | 'Love' | 'Health' | 'Learn' | 'Fun' | 'Wealth'
+      format: 'Audio' | 'Video' | 'Automatic' | 'Focused'
+      circle: 'Solo' | 'Jenny' | 'Group'
+      link?: string
+      rank?: number
+      parentTitle?: string
+    }
+
+const getParentAchievementId = async (
+  achievement: Achievement
+): Promise<string | null> => {
+  if (!achievement.parentTitle) return null
+
+  /* Find the parent achievement */
+  const parentResponse = await notion.databases.query({
+    database_id: ACHIEVEMENTS_DATABASE_ID,
+    filter: {
+      property: 'title',
+      rich_text: { equals: achievement.parentTitle },
+    },
+  })
+  const parentExists = parentResponse.results.length > 0
+  if (achievement.parentTitle && !parentExists)
+    throw new Error('Parent not found')
+  return parentResponse.results[0].id
+}
+
+export const findOrCreateNotionAchievement = async (
+  achievement: Achievement
+): Promise<void> => {
+  /* Check if the achievement page already exists */
+  const response = await notion.databases.query({
+    database_id: ACHIEVEMENTS_DATABASE_ID,
+    filter: {
+      property: 'title',
+      rich_text: { equals: achievement.title },
+    },
+  })
+  const alreadyExists = response.results.length > 0
+  if (alreadyExists) return
+
+  /* Find the parent achievement */
+  const parentAchievementId = await getParentAchievementId(achievement)
+
+  /* If the achievement doesn't exist, add it */
+  await notion.pages.create({
+    parent: { database_id: ACHIEVEMENTS_DATABASE_ID },
+    properties: {
+      Name: {
+        type: 'title',
+        title: [{ type: 'text', text: { content: achievement.title } }],
+      },
+      Target: {
+        type: 'number',
+        number: achievement.target ?? 0,
+      },
+      Progress: {
+        type: 'number',
+        number: achievement.progress ?? 0,
+      },
+      Type: {
+        type: 'select',
+        select: { name: achievement.type },
+      },
+      Category: {
+        type: 'select',
+        select: { name: achievement.category },
+      },
+      Format: {
+        type: 'select',
+        select: { name: achievement.format },
+      },
+      Circle: {
+        type: 'select',
+        select: { name: achievement.circle },
+      },
+      ...(achievement.link
+        ? { Link: { type: 'url', url: achievement.link } }
+        : {}),
+      ...(parentAchievementId
+        ? {
+            Parent: {
+              type: 'relation',
+              relation: [{ id: parentAchievementId }],
+            },
+          }
+        : {}),
+    },
+  })
+}
+
+/* Previous function that saves achievements to the database, remove once it's no longer called */
 export const findOrCreateAchievementByTitle = async ({
   title,
   type,
@@ -40,20 +161,4 @@ export const findOrCreateAchievementByTitle = async ({
     description,
     link,
   })
-  // .catch((error) => {
-  //   // eslint-disable-next-line no-console
-  //   console.log(error)
-  //   return findOrCreateAchievementByTitle({
-  //     title,
-  //     type,
-  //     categoryName,
-  //     formatName,
-  //     circleName,
-  //     parentAchievementId,
-  //     target,
-  //     level,
-  //     description,
-  //     link,
-  //   })
-  // })
 }
