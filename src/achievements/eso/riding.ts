2f8712d89373c855090ff3ddbcb793aa27dba05c
Alan Walton <aawalton@gmail.com>
2023-06-06 07:59:34 -0600

    add script for riding achievements

---
+++ src/achievements/eso/riding.ts
@@ -0,0 +1,50 @@
+/* eslint-disable no-console */
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-restricted-syntax */
+import database from '../../modules/database'
+import { findOrCreateNotionAchievement } from '../helpers'
+
+const sharedAttributes = {
+  category: 'Fun',
+  format: 'Automatic',
+  circle: 'Solo',
+  tags: ['Elder Scrolls Online'],
+} as const
+
+const createCharacterAchievements = async () => {
+  /* Find or create category achievement */
+  const categoryTitle = 'Complete Elder Scrolls Online'
+  await findOrCreateNotionAchievement({
+    title: categoryTitle,
+    type: 'Collection',
+    ...sharedAttributes,
+  })
+
+  /* Find or create parent achievement */
+  const parentTitle = 'Max Riding Skill for All Characters'
+  await findOrCreateNotionAchievement({
+    title: parentTitle,
+    type: 'Collection',
+    parentTitle: categoryTitle,
+    ...sharedAttributes,
+  })
+
+  /* Find or create achievements for characters */
+  const getCharacterTitle = (characterName: string) =>
+    `Max Riding Skill for ${characterName}`
+  const characters = await database.esoCharacters.findAll()
+  await Promise.all(
+    characters.map((character) =>
+      findOrCreateNotionAchievement({
+        title: getCharacterTitle(character.name),
+        type: 'Integer',
+        target: 180,
+        parentTitle,
+        rank: character.rank,
+        ...sharedAttributes,
+      })
+    )
+  )
+}
+
+void createCharacterAchievements()
